ecbc707fdaea4c27ca88433213a391576e9ca683
linkeo <linkeo2012@gmail.com>
2025-12-14 21:44:57 +0800

    chore: fix home redirect

--- src/routes/home.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "react-router";
-
-export default function Home() {
-  return redirect("/book/CET4luan_2");
-}
--- src/routes/home/loader.ts
+++ src/routes/home/loader.ts
@@ -2,5 +2,5 @@ import { type LoaderFunctionArgs, redirect } from "react-router";
 
 export async function homeLoader({ params }: LoaderFunctionArgs) {
   console.log("home loader", params);
-  return redirect("/book/CET4_2");
+  return redirect("/book/CET4luan_2");
 }
